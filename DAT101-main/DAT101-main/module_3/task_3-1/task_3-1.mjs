691ff0019ea8fb35719f637c017de7caa2c6428d
haron <haronmh@uia.no>
2024-10-31 11:48:27 +0100

    3.1

--- DAT101-main/DAT101-main/module_3/task_3-1/task_3-1.mjs
+++ DAT101-main/DAT101-main/module_3/task_3-1/task_3-1.mjs
@@ -5,39 +5,107 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+printOut(newLine);
+
+let wakeUpTime = 8;
+const ableBusTime = 7;
+const ableTrainTime = 8;
+
+if (wakeUpTime == ableBusTime) {
   printOut("I can take the bus to school.");
+}else if(wakeUpTime == ableTrainTime){
+printOut("if I wake up at exactly 8 o'clock i'll be able to take the train, if not then the care has to be taken...")
+}else{
+  printOut("if i wake up at exactly 7 o'clock ill be able to take the but, or if i wake up at exactly 8 i'll be able to take the train," +
+    " if im not able to wake up at neither then ill have to take the car :(")
 }
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const int1 = 10+2;
+const int2 = 10+3;
+
+if(int1 < int2){
+  printOut("positive")
+}else if(int1 == int2){
+printOut("ZEROOOOOO")
+}else(printOut("Negative"));
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut(newLine);
+
+const minSizeOfFile = 4;
+const fileSize1 = Math.floor(Math.random()*8 + 1);
+
+printOut(fileSize1 + " is the file size");
+
+if(fileSize1 > minSizeOfFile){
+  printOut("Thank you")
+}else(printOut("Your file is too small..."));
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut(newLine);
+
+const fileSize2 = Math.floor(Math.random()*8 + 1);
+const maxSizeOfFile = 6;
+
+printOut(fileSize2 + " is the file size");
+
+if(fileSize2 > maxSizeOfFile){
+  printOut("Your file is too large :(")
+}else if(fileSize2 < minSizeOfFile){
+  printOut("File is too small :(")
+}else(printOut("Thank you :)"));
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthList =["January", "February", "Mars", "April", "Mai",
+  "Jun", "Juli", "August", "September", "October", "November", "December"];
+  const noOfMonth = monthList.length;
+  const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+
+  let includeR = monthName.indexOf("r").toString()
+  printOut("Month is = " + monthName)
+
+if(includeR = "r"){
+  printOut("You must take vitamin D")
+}else(printOut("You do not need to take vitamin D"))
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+if(monthName == monthList[3,5,8,10]){
+  printOut("there are 30 days in " + monthName.toString())
+}else if(monthName == monthList[1]){
+  printOut("There are 28 days in " + monthName.toString() + " during a normal year")
+}else(printOut("There are 31 days in " + monthName.toString()))
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+
+if(monthName == monthList[2,4]){
+  printOut("The art gallery is unfortunately closed :(");
+}else if(monthName == monthList[3]){
+  printOut("The art gallery is partially open :)");
+}else(printOut("The art gallery is OPEN!! :)"));
+
 printOut("Replace this with you answer!");
 printOut(newLine);
--- DAT101-main/DAT101-main/module_3/task_3-2/task_3-2.mjs
+++ DAT101-main/DAT101-main/module_3/task_3-2/task_3-2.mjs
@@ -4,32 +4,111 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut(newLine);
+
+for(let count110 = 1; count110 <= 10; count110++){
+    printOut(count110.toString())
+}
+for(let count101 = 10; count101 >= 1; count101--){
+    printOut(count101.toString())
+}
+
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut(newLine);
+
+const targetNR = 45;
+let guessedNumber = 0; 
+
+while(targetNR !== guessedNumber){
+    guessedNumber = Math.floor((Math.random() * 60) + 1)
+}
+printOut(guessedNumber + newLine)
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut(newLine);
+
+guessedNumber = 0;
+let guesses = 0;
+const startTime = Date.now();
+while (targetNR !== guessedNumber){
+    guesses++;
+    guessedNumber = Math.floor((Math.random() * 1000000) + 1);
+}
+const endTime = Date.now();
+const timeUsed = endTime - startTime;
+
+printOut("antall gjettinger = " + guesses + " in " + timeUsed + " ms")
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+printOut(newLine);  
+
+function isPrime(num) {
+    if (num < 2) return false; 
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false; 
+    }
+    return true; // is prime
+}
+
+
+let primeNumbers = [];
+for (let num = 1; num <= 200; num++) {
+    if (isPrime(num)) {
+        primeNumbers.push(num);
+    }
+}
+
+printOut(primeNumbers + newLine);
+
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut(newLine);
+
+for(let row = 1; row <= 7; row++){
+    let rowText = "";
+    for(let column = 1; column <= 9; column++){
+        rowText += "K" + column + "R" + row + "";
+    }
+    printOut(rowText);
+}
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut(newLine);
+
+const points = Math.floor((Math.random() * 236) + 1);
+const percent = points / 236 * 100;
+let grade;
+
+if(percent < 40){
+    grade = "F";
+}else if(percent < 52){
+    grade = "E";
+}else if(percent < 64){
+    grade = "D";
+}else if(percent < 76){
+    grade = "C";
+}else if(percent < 88){
+    grade = "B";
+}else {
+    grade = "A"
+}
+printOut("The student got " + grade.toString() + " with " + Math.round(percent) +  "% and " + points + " points");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
